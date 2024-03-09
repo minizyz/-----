c9c7b7796cf51b16f97881dc5d70a2ad9327dda9
陈佳鑫 <jiaxinchen@outlook.com>
2024-03-09 19:56:43 +0800

    On branch main
     Initial commit
    
     Changes to be committed:
    	new file:   "\346\225\260\346\215\256\347\273\223\346\236\204/\351\223\276\350\241\250/\345\215\225\351\223\276\350\241\250/Main.h"

---
+++ 数据结构/链表/单链表/Main.h
@@ -0,0 +1,72 @@
+#include <bits/stdc++.h>
+#include "数据结构\Common.h"
+using namespace std;
+typedef struct Node
+{
+	int data;
+	struct Node *next;
+} Node;
+typedef struct Node *LinkList;
+Status GetElem(LinkList L, int i, int *e)
+{
+	int j;
+	LinkList p;
+	p = L->next;
+	j = 1;
+	while (p && j < i)
+	{
+		p = p->next;
+		++j;
+	}
+	if (!p || j > i)
+	{
+		return ERROR;
+	}
+	*e = p->data;
+	return OK;
+}
+Status ListInsert(LinkList *L, int i, int e)
+{
+	int j;
+	LinkList p, s;
+	p = *L;
+	j = 1;
+	while (p && j < i)
+	{
+		p = p->next;
+		++j;
+	}
+	if (!p || j > i)
+	{
+		return ERROR;
+	}
+	s = (LinkList)malloc(sizeof(Node));
+	s->data = e;
+	s->next = p->next;
+	p->next = s;
+	return OK;
+}
+Status ListDelete(LinkList *L, int i, int *e)
+{
+	int j;
+	LinkList p, q;
+	p = *L;
+	j = 1;
+	while (p->next && j < i)
+	{
+		p = p->next;
+		++j;
+	}
+	if (!(p->next) || j > i)
+	{
+		return ERROR;
+	}
+	q = p->next;
+	p->next = q->next;
+	*e = q->data;
+	free(q);
+	return OK;
+}
+void CreateList(LinkList *L,int n){
+
+}
